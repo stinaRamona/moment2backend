3edc87afdab31940174c2e1106916ab96bc8b5f8
Stina Persson <stpe1901@student.miun.se>
2024-04-11 10:37:45 +0200

    Lagt till snyggare felmeddelanden vid fel under POST anrop. Samt att värden hämtas från body

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const db = new sqlite3.Database("./db/experience.db");
 app.use(express.json()); 
 app.use(cors()); 
 
-//routing 
+//routing för att hämta
 app.get('/api', (req, res) =>{
     res.json({message: 'Hej. välkommen till mitt API'}); 
 }); 
@@ -18,18 +18,58 @@ app.get('/api/workexp', (req, res) => {
     res.json({message: 'Tidigare erfarenheter:'})
 }); 
 
-app.post('/api/workexp', (req, res) => {
-    res.json({message: 'POST förfrågan'}); 
+//För att lägga till
+app.post('/api/workexp', (req, res) => { 
+
+    //variabler sparas från body
+    let company = req.body.company; 
+    let title = req.body.title; 
+    let description = req.body.description; 
+    let location = req.body.location; 
+
+    //värden från body sparas i objekt
+    let workexp = {
+        company: company,
+        title: title, 
+        description: description, 
+        location: location
+    }; 
+
+    //objekt för felmeddelanden 
+    let errors = {
+        message: "", 
+        details: "", 
+        http_response: { }
+    }
+
+    //Ifall inte alla värden är ifyllda hamnar vi här 
+    if(!company || !title || !description || !location) {
+        errors.message = "Alla värden är inte medskickade"; 
+        errors.details = "Du måste ange alla värden för att kunna spara ny erfarenhet";  
+
+        errors.http_response.message = "Bad request"; 
+        errors.http_response.code = 400; 
+
+        res.status(400).json({errors})
+
+        return; 
+    }
+
+    //annars postas att förfrågan är godkänd och det nya objektet 
+    res.json({message: 'POST förfrågan godkänd', workexp}); 
 });
 
-app.put('/api/workexp', (req, res) => {
+//för att uppdatera en post
+app.put('/api/workexp/:id', (req, res) => {
     res.json({message: 'Uppdatering av post med id:' + req.params.id}); 
 });
 
-app.delete('/api/workexp', (req, res) => {
+//för att radera en post 
+app.delete('/api/workexp/:id', (req, res) => {
     res.json({message: 'Tar bort post med id:' + req.params.id}); 
 }); 
 
+//sätter igång servern
 app.listen(port, () => {
     console.log(`servern är startad på port:${port}`); 
 })
